6d3d516a95c713adfa4bae4c4765661d6013e443
Lucas <englucas@outlook.com>
2018-10-05 13:45:04 +0000

    Start

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Arquivos SASS
+import './scss/index.scss';
+
+// Dependencias
+import 'jquery';
+import 'bootstrap';
+
+// Mues arquivos JS
+import './js/core/includes';
+import './js/plugins/cityButtons';
+
